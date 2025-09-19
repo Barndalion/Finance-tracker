933ebd1d716a02eae32b62a2ca886b3271bd47ad
barndalion <darellbarnes450@gmail.com>
2025-09-19 14:10:17 -0400

    Fixed bug with metadata file, 8 is now at the start

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
-import datetime
-from config import *
-from utils import *
-
-print(get_balance("test"))
+class Solution:
+    def removeDuplicates(self, nums):
+        encountered = []
+        i = 0
+        while i < len(nums):
+            if nums[i] not in encountered:
+                encountered.append(nums[i])
+            if nums[i] in encountered:
+                nums.pop(i)
+        return len(encountered),encountered
+    
+sol = Solution()
+s = [1,2,2,2,3,4,5,6,6,6,7,8,9,9,9,10]
+print(sol.removeDuplicates(s))
